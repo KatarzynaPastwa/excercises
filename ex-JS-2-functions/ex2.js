d93ff39e8086fe1ba08ca6a4e3521e81074b8d92
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-06 00:13:01 +0200

    Exercises form JS-2 done.

--- ex-JS-2-functions/ex2.js
+++ ex-JS-2-functions/ex2.js
@@ -34,23 +34,31 @@ function greeting(){
 function loveGreeting(){
 
     function greeting(){
-        var firstName="Ola"
-        var lastName="Kowalska"
-        return ("Hello my dear ", firstName, " ", lastName, "!")
-    }
+        var firstName="Ola";
+        var lastName="Kowalska";
+        return ("Hello my dear " + firstName + "  "+ lastName + " !");
+    };
 
     console.log(greeting(), "I love You")
-}
+};
 
 
 // TODO: 4. Opakuj kod z tego pliku w IIFE.
 
-
+(function(){cały_kod_tego_pliku_tu_w_Środku})()
 
 
 
 // TODO: 5. Stwórz interwał, ktory co 1 wyswietli w konsoli console.log("Hello!"). Dodatkowo, po 5 sekundach interwal powinien przestac pracowac
 // nalezy wykorzystac funkcje setInterval, clearInterval, setTimeout
+// naprawienie za pomoca domkniecia oraz IIFE
+for (var i = 0; i < 5; i++) {
+    (function(x) {
+      setInterval(function() {
+        console.log("Hello!")
+      }, x * 1000 + 1000)
+    })(i)
+  }
 
 
 
