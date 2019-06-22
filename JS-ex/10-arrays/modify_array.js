98c384b050d7066217c5a39e5274ee16510a65a4
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-22 14:42:21 +0200

    ex. 10-arrays-modufy_arrays done

--- JS-ex/10-arrays/modify_array.js
+++ JS-ex/10-arrays/modify_array.js
@@ -1,11 +1,25 @@
 var names = ['Janek', 'Bolek', 'Rysiu'];
 
 // 1. TODO. Do tablicy names dodaj parę imion na koniec tablicy
+
+names.push("Ola", "Ala")
+
 // 2. TODO. Z tablicy names usuń imię z końca tablicy
+
+names.pop()
+
 // 3. TODO. Z tablicy names usuń imię z początku tablicy
+
+names.shift()
+
 // 4. TODO. Do tablicy names dodaj parę imion na początek tablicy
+
+names.unshift("Roland", "Grzesiu", "Kazik")
 
 // 5. TODO: Do tablicy names, dołącz tablicę englishNames (zmodyfikuj)
 var englishNames = ['Monica', 'Jessica', 'John'];
 
+names=names.concat(names, englishNames)
 // 6. TODO: Skopiuj tablicę names bez referencji
+
+console.log(names.join(", "))
