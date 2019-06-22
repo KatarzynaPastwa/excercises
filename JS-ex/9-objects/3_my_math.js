c0f52c0b0d998e5f57a3587d3c8f48e0691a8a22
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-22 13:10:30 +0200

    ex. 9/3/1 done

--- JS-ex/9-objects/3_my_math.js
+++ JS-ex/9-objects/3_my_math.js
@@ -4,3 +4,21 @@ wzory:  peekY = - delta/4a
         delta = b * b - 4 * a * c
         Wykorzystaj stworzony obiekt i wywołaj jego metody z odpowiednimi parametrami, sprwadz czy zwraca dobre wartości
  */
+var a=1;
+var b=2;
+var c=3;
+var delta = b * b - 4 * a * c;
+var MyMath={
+
+    peekY: function(){
+        let pY=(delta/4*a);
+        console.log(pY)
+    };
+    peekX: function(){
+        let pX=(a / 2*b);
+        console.log(pX)
+    };
+    delta: function(){
+        console.log(delta);
+    };
+}
