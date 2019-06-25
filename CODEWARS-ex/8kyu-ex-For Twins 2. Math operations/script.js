502202cfc476f876523a63dda7990297991932e9
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-25 13:47:03 +0200

    cleannig staff

--- CODEWARS-ex/8kyu-ex-For Twins 2. Math operations/script.js
+++ CODEWARS-ex/8kyu-ex-For Twins 2. Math operations/script.js
@@ -1,17 +1,7 @@
-
-
-
-
-
-
 function iceBrickVolume(radius, bottleLength, rimLength) {
-
 
   const iceBrickVolume = radius * radius * (bottleLength - rimLength) * 2;
   return iceBrickVolume
   }
-
-
-
 
   iceBrickVolume(1, 10, 2)
