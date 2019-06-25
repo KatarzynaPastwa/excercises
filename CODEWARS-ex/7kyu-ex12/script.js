16ddc426989db63d6ea993de9b9e28e66b19d6fb
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-25 17:29:50 +0200

    ex 12 done

---
+++ CODEWARS-ex/7kyu-ex12/script.js
@@ -0,0 +1,29 @@
+
+[
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' }
+]
+
+
+
+
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+
+
+function findAdmin(list, lang) {
+  // thank you for checking out the Coding Meetup kata :)
+
+  const odp = list.filter(function(user){
+   if( user.language=== lang && user.githubAdmin === 'yes') {
+     return user
+   }
+   
+  })
+return odp
+}
