6aa0ba6e512459d7f74453467c63f2f6ecf2f328
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-06-25 16:59:19 +0200

    ex. 10 done

--- CODEWARS-ex/7kyu-ex10/script.js
+++ CODEWARS-ex/7kyu-ex10/script.js
@@ -1,22 +1,26 @@
+
+[
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
+    username: 'emilyn1990' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', 
+    username: 'nore2000' }
+]
+
+
+
+
+
+
 var list1 = [
-  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
-  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
-  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
-  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
-  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
 ];
 
-function allContinents(list) {
-  
-  return list.some(user=>user.age <20) &&
-         list.some(user=>user.age >=20 && user.age<30) &&
-         list.some(user=>user.age >=30 && user.age<40) &&
-         list.some(user=>user.age >=40 && user.age<50) &&
-         list.some(user=>user.age >=50 && user.age<60) &&
-         list.some(user=>user.age >=60 && user.age<70) &&
-         list.some(user=>user.age >=70 && user.age<80) &&
-         list.some(user=>user.age >=80 && user.age<90) &&
-         list.some(user=>user.age >=90 && user.age<100) &&
-         list.some(user=>user.age >=100);
+function addUsername(list, year = new Date().getFullYear()) {
 
-}
+  return list.map(function(user) {
+    user.username = user.firstName.toLowerCase() + user.lastName[0].toLowerCase()+(year - user.age);
+    return user;
+  });
+   
+ }
